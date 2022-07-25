d62a9aa4ee59c7719c1c061fe0345e375ae06a00
Steve Bachmeier <sbachmei@uw.edu>
2022-07-25 11:46:23 -0700

    Update pins

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
         long_description = f.read()
 
     install_requirements = [
-        'vivarium==0.10.16',
-        'vivarium_public_health==0.10.19',
+        'vivarium==0.10.17',
+        'vivarium_public_health==0.10.20',
 
         'click',
         'gbd_mapping>=3.0.0, <4.0.0',
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     # use "pip install -e .[dev]" to install required components + extra components
     extras_require = [
         'vivarium_cluster_tools==1.3.4',
-        'vivarium_inputs[data]==4.0.7',
+        'vivarium_inputs[data]==4.0.8',
     ]
 
     setup(
