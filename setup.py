61e56bfd5a41bce578ead77ddae8fa3f802eb31f
Steve Bachmeier <sbachmei@uw.edu>
2022-07-27 10:00:01 -0700

    black

--- setup.py
+++ setup.py
@@ -17,56 +17,49 @@ if __name__ == "__main__":
         long_description = f.read()
 
     install_requirements = [
-        'gbd_mapping==3.0.6',
-        'vivarium==0.10.17',
-        'vivarium_public_health==0.10.20',
-
-        'click',
-        'jinja2',
-        'loguru',
-        'numpy',
-        'pandas',
-        'scipy',
-        'tables',
-        'pyyaml',
+        "gbd_mapping==3.0.6",
+        "vivarium==0.10.17",
+        "vivarium_public_health==0.10.20",
+        "click",
+        "jinja2",
+        "loguru",
+        "numpy",
+        "pandas",
+        "scipy",
+        "tables",
+        "pyyaml",
     ]
 
     # use "pip install -e .[dev]" to install required components + extra components
     data_requires = [
-        'vivarium_cluster_tools==1.3.4',
-        'vivarium_inputs[data]==4.0.8',
+        "vivarium_cluster_tools==1.3.4",
+        "vivarium_inputs[data]==4.0.8",
     ]
 
-    test_requirements = ['pytest']
+    test_requirements = ["pytest"]
 
     setup(
-        name=about['__title__'],
-        version=about['__version__'],
-
-        description=about['__summary__'],
+        name=about["__title__"],
+        version=about["__version__"],
+        description=about["__summary__"],
         long_description=long_description,
-        license=about['__license__'],
+        license=about["__license__"],
         url=about["__uri__"],
-
         author=about["__author__"],
         author_email=about["__email__"],
-
-        package_dir={'': 'src'},
-        packages=find_packages(where='src'),
+        package_dir={"": "src"},
+        packages=find_packages(where="src"),
         include_package_data=True,
-
         install_requires=install_requirements,
         extras_require={
             "test": test_requirements,
             "data": data_requires,
             "dev": test_requirements + data_requires,
         },
-
         zip_safe=False,
-
-        entry_points='''
+        entry_points="""
             [console_scripts]
             make_artifacts=vivarium_nih_us_cvd.tools.cli:make_artifacts
             make_results=vivarium_nih_us_cvd.tools.cli:make_results
-        '''
+        """,
     )
--- src/vivarium_nih_us_cvd/constants/models.py
+++ src/vivarium_nih_us_cvd/constants/models.py
@@ -2,11 +2,10 @@ from vivarium_nih_us_cvd.constants import data_keys
 
 
 class TransitionString(str):
-
     def __new__(cls, value):
         # noinspection PyArgumentList
         obj = str.__new__(cls, value.lower())
-        obj.from_state, obj.to_state = value.split('_TO_')
+        obj.from_state, obj.to_state = value.split("_TO_")
         return obj
 
 
@@ -15,27 +14,35 @@ class TransitionString(str):
 ###########################
 
 ISCHEMIC_STROKE_MODEL_NAME = data_keys.ISCHEMIC_STROKE.name
-ISCHEMIC_STROKE_SUSCEPTIBLE_STATE_NAME = f'susceptible_to_{ISCHEMIC_STROKE_MODEL_NAME}'
-ACUTE_ISCHEMIC_STROKE_STATE_NAME = 'acute_ischemic_stroke'
-CHRONIC_ISCHEMIC_STROKE_STATE_NAME = 'chronic_ischemic_stroke'
+ISCHEMIC_STROKE_SUSCEPTIBLE_STATE_NAME = f"susceptible_to_{ISCHEMIC_STROKE_MODEL_NAME}"
+ACUTE_ISCHEMIC_STROKE_STATE_NAME = "acute_ischemic_stroke"
+CHRONIC_ISCHEMIC_STROKE_STATE_NAME = "chronic_ischemic_stroke"
 ISCHEMIC_STROKE_MODEL_STATES = (
     ISCHEMIC_STROKE_SUSCEPTIBLE_STATE_NAME,
     ACUTE_ISCHEMIC_STROKE_STATE_NAME,
     CHRONIC_ISCHEMIC_STROKE_STATE_NAME,
 )
 ISCHEMIC_STROKE_MODEL_TRANSITIONS = (
-    TransitionString(f'{ISCHEMIC_STROKE_SUSCEPTIBLE_STATE_NAME}_TO_{ACUTE_ISCHEMIC_STROKE_STATE_NAME}'),
-    TransitionString(f'{ACUTE_ISCHEMIC_STROKE_STATE_NAME}_TO_{CHRONIC_ISCHEMIC_STROKE_STATE_NAME}'),
-    TransitionString(f'{CHRONIC_ISCHEMIC_STROKE_STATE_NAME}_TO_{ACUTE_ISCHEMIC_STROKE_STATE_NAME}'),
+    TransitionString(
+        f"{ISCHEMIC_STROKE_SUSCEPTIBLE_STATE_NAME}_TO_{ACUTE_ISCHEMIC_STROKE_STATE_NAME}"
+    ),
+    TransitionString(
+        f"{ACUTE_ISCHEMIC_STROKE_STATE_NAME}_TO_{CHRONIC_ISCHEMIC_STROKE_STATE_NAME}"
+    ),
+    TransitionString(
+        f"{CHRONIC_ISCHEMIC_STROKE_STATE_NAME}_TO_{ACUTE_ISCHEMIC_STROKE_STATE_NAME}"
+    ),
 )
 
 STATE_MACHINE_MAP = {
     ISCHEMIC_STROKE_MODEL_NAME: {
-        'states': ISCHEMIC_STROKE_MODEL_STATES,
-        'transitions': ISCHEMIC_STROKE_MODEL_TRANSITIONS,
+        "states": ISCHEMIC_STROKE_MODEL_STATES,
+        "transitions": ISCHEMIC_STROKE_MODEL_TRANSITIONS,
     },
 }
 
 
-STATES = tuple(state for model in STATE_MACHINE_MAP.values() for state in model['states'])
-TRANSITIONS = tuple(state for model in STATE_MACHINE_MAP.values() for state in model['transitions'])
+STATES = tuple(state for model in STATE_MACHINE_MAP.values() for state in model["states"])
+TRANSITIONS = tuple(
+    state for model in STATE_MACHINE_MAP.values() for state in model["transitions"]
+)
