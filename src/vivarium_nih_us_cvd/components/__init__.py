c56a6284c2d472cf8ec96f0b5f5a7abe6796b521
Steve Bachmeier <sbachmei@uw.edu>
2022-09-16 11:51:00 -0700

    Undo observer mods

--- src/vivarium_nih_us_cvd/components/__init__.py
+++ src/vivarium_nih_us_cvd/components/__init__.py
@@ -1,8 +1,3 @@
 from .disease import IschemicStroke, MyocardialInfarction
-from .healthcare_visits import HealthcareVisits
-from .observers import (
-    ContinuousRiskObserver,
-    HealthcareVisitObserver,
-    ResultsStratifier,
-)
+from .observers import ContinuousRiskObserver, ResultsStratifier
 from .risks import Risk
--- src/vivarium_nih_us_cvd/components/observers.py
+++ src/vivarium_nih_us_cvd/components/observers.py
@@ -2,13 +2,10 @@ from collections import Counter
 from typing import Dict
 
 from vivarium.framework.engine import Builder
-from vivarium.framework.time import get_time_stamp
 from vivarium_public_health.metrics.stratification import (
     ResultsStratifier as ResultsStratifier_,
 )
 from vivarium_public_health.utilities import EntityString, to_years
-
-from vivarium_nih_us_cvd.constants import data_values
 
 
 class ResultsStratifier(ResultsStratifier_):
@@ -98,79 +95,6 @@ class ContinuousRiskObserver:
         for label, group_mask in groups:
             key = f"total_exposure_time_risk_{self.risk.name}_{label}"
             new_observations[key] = values[group_mask].sum() * step_size_in_years
-
-        self.counter.update(new_observations)
-
-    def metrics(self, index: "pd.Index", metrics: Dict[str, float]) -> Dict[str, float]:
-        metrics.update(self.counter)
-        return metrics
-
-
-class HealthcareVisitObserver:
-    """Observes doctor visit counts per group."""
-
-    configuration_defaults = {
-        "observers": {
-            "visits": {
-                "exclude": [],
-                "include": [],
-            }
-        }
-    }
-
-    def __init__(self):
-        self.configuration_defaults = self._get_configuration_defaults()
-
-    def __repr__(self):
-        return f"HealthcareVisitObserver"
-
-    ##########################
-    # Initialization methods #
-    ##########################
-
-    def _get_configuration_defaults(self) -> Dict[str, Dict]:
-        return HealthcareVisitObserver.configuration_defaults
-
-    ##############
-    # Properties #
-    ##############
-
-    @property
-    def name(self):
-        return f"healthcare_visit_observer"
-
-    #################
-    # Setup methods #
-    #################
-
-    def setup(self, builder: Builder) -> None:
-        self.observation_start_time = get_time_stamp(builder.configuration.time.observation_start)
-        self.config = self._get_stratification_configuration(builder)
-        self.stratifier = builder.components.get_component(ResultsStratifier.name)
-
-        self.counter = Counter()
-
-        columns_required = [data_values.VISIT_TYPE_COLUMN]
-        self.population_view = builder.population.get_view(columns_required)
-
-        builder.event.register_listener("collect_metrics", self.on_collect_metrics)
-        builder.value.register_value_modifier("metrics", self.metrics)
-
-    def _get_stratification_configuration(self, builder: Builder) -> "ConfigTree":
-        return builder.configuration.observers["visits"]
-
-    def on_collect_metrics(self, event: "Event"):
-        # TODO: Confirm this is correct - it might be off by one
-        if event.time < self.observation_start_time:
-            return
-        pop = self.population_view.get(event.index, query='alive == "alive"')
-
-        new_observations = {}
-        groups = self.stratifier.group(pop.index, self.config.include, self.config.exclude)
-        for label, group_mask in groups:
-            for visit_type in data_values.VISIT_TYPES:
-                key = f"healthcare_visits_{visit_type}_{label}"
-                new_observations[key] = pop[group_mask].squeeze().str.contains(visit_type).sum()
 
         self.counter.update(new_observations)
 
