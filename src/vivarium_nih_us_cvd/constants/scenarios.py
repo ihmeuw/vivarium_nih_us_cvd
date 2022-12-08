9a8a486a32adafd08af8dd3ff9f916812e7c19b4
sbachmei <sbachmei@uw.edu>
2022-12-08 16:30:18 -0500

    define scenarios is_polypill_scenario based on effects

--- src/vivarium_nih_us_cvd/constants/scenarios.py
+++ src/vivarium_nih_us_cvd/constants/scenarios.py
@@ -10,56 +10,53 @@ class InterventionScenario:
         self,
         name: str,
         is_outreach_scenario: bool = False,
-        is_polypill_scenario: bool = False,
-        affect_sbp_adherence: bool = False,
-        affect_sbp_medication: bool = False,
+        polypill_affects_sbp_adherence: bool = False,
+        polypill_affects_sbp_medication: bool = False,
     ):
         self.name = name
         self.is_outreach_scenario = is_outreach_scenario
-        self.is_polypill_scenario = is_polypill_scenario
-        self.affect_sbp_adherence = affect_sbp_adherence
-        self.affect_sbp_medication = affect_sbp_medication
+        self.polypill_affects_sbp_adherence = polypill_affects_sbp_adherence
+        self.polypill_affects_sbp_medication = polypill_affects_sbp_medication
+        self.is_polypill_scenario = (
+            polypill_affects_sbp_adherence or polypill_affects_sbp_medication
+        )
 
 
 class __InterventionScenarios(NamedTuple):
     BASELINE: InterventionScenario = InterventionScenario("baseline")
     OUTREACH_50: InterventionScenario = InterventionScenario(
-        "outreach_50", is_outreach_scenario=True
+        "outreach_50",
+        is_outreach_scenario=True,
     )
     OUTREACH_100: InterventionScenario = InterventionScenario(
-        "outreach_100", is_outreach_scenario=True
+        "outreach_100",
+        is_outreach_scenario=True,
     )
     POLYPILL_50: InterventionScenario = InterventionScenario(
         "polypill_50",
-        is_polypill_scenario=True,
-        affect_sbp_adherence=True,
-        affect_sbp_medication=True,
+        polypill_affects_sbp_adherence=True,
+        polypill_affects_sbp_medication=True,
     )
     POLYPILL_100: InterventionScenario = InterventionScenario(
         "polypill_100",
-        is_polypill_scenario=True,
-        affect_sbp_adherence=True,
-        affect_sbp_medication=True,
+        polypill_affects_sbp_adherence=True,
+        polypill_affects_sbp_medication=True,
     )
     POLYPILL_ADHERENCE_ONLY_50: InterventionScenario = InterventionScenario(
         "polypill_adherence_only_50",
-        is_polypill_scenario=True,
-        affect_sbp_adherence=True,
+        polypill_affects_sbp_adherence=True,
     )
     POLYPILL_ADHERENCE_ONLY_100: InterventionScenario = InterventionScenario(
         "polypill_adherence_only_100",
-        is_polypill_scenario=True,
-        affect_sbp_adherence=True,
+        polypill_affects_sbp_adherence=True,
     )
     POLYPILL_MEDICATION_ONLY_50: InterventionScenario = InterventionScenario(
         "polypill_medication_only_50",
-        is_polypill_scenario=True,
-        affect_sbp_medication=True,
+        polypill_affects_sbp_medication=True,
     )
     POLYPILL_MEDICATION_ONLY_100: InterventionScenario = InterventionScenario(
         "polypill_medication_only_100",
-        is_polypill_scenario=True,
-        affect_sbp_medication=True,
+        polypill_affects_sbp_medication=True,
     )
 
     def __getitem__(self, item) -> InterventionScenario:
