edc620c095a2f5f0594880322a49f4b954cfc0ee
hussain-jafari <hussainsjafari@gmail.com>
2023-02-24 11:30:18 -0800

    paf observer

--- src/vivarium_nih_us_cvd/components/__init__.py
+++ src/vivarium_nih_us_cvd/components/__init__.py
@@ -3,10 +3,7 @@ from .effects import InterventionAdherenceEffect
 from .healthcare_utilization import HealthcareUtilization
 from .interventions import LinearScaleUp
 from .observers import (
-    CategoricalColumnObserver,
-    ContinuousRiskObserver,
-    HealthcareVisitObserver,
-    ResultsStratifier,
+    PAFObserver,
 )
 from .risks import AdjustedRisk, TruncatedRisk
 from .treatment import Treatment
