57d9a996087a44c363f5ea899ec2cac36e52b68c
hussain-jafari <hussainsjafari@gmail.com>
2023-02-28 09:53:42 -0800

    address PR comments

--- src/vivarium_nih_us_cvd/components/paf_observer.py
+++ src/vivarium_nih_us_cvd/components/paf_observer.py
@@ -29,19 +29,19 @@ class PAFObserver:
 
     # noinspection PyAttributeOutsideInit
     def setup(self, builder: Builder) -> None:
-        self.step_size = builder.time.step_size()
-        self.config = builder.configuration.stratification["paf"]
         self.risk_effect = builder.components.get_component(
             f"risk_effect.{self.risk}.{self.target}"
         )
 
+        config = builder.configuration.stratification["paf"]
+
         builder.results.register_observation(
             name=f"calculated_paf_{self.risk}_on_{self.target}",
-            pop_filter=f'alive == "alive"',
+            pop_filter='alive == "alive"',
             aggregator=self.calculate_paf,
             requires_columns=["alive"],
-            additional_stratifications=self.config.include,
-            excluded_stratifications=self.config.exclude,
+            additional_stratifications=config.include,
+            excluded_stratifications=config.exclude,
             when="time_step__prepare",
         )
 
@@ -55,6 +55,6 @@ class PAFObserver:
     def get_configuration_defaults(self) -> Dict[str, Dict]:
         return {
             "stratification": {
-                self.risk: PAFObserver.configuration_defaults["stratification"]["paf"]
+                self.risk: PAFObserver.configuration_defaults["stratification"][f"{self.risk}_paf"]
             }
         }
