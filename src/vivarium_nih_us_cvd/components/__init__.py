0484c4f1aa5626ba100edd2cf0863e2bde730a2a
Hussain Jafari <hussainsjafari@gmail.com>
2023-02-28 11:22:46 -0800

    PAF observer (#85)
        Category: observer
        JIRA issue: MIC-3822
    
    Verification and Testing
    Put a breakpoint in the aggregator function and checked that relative risk values were around what we would expect for demographic groups (looking at age) and checked that calculated PAF values in output of sim run looked reasonable. Checks were done for BMI and FPG on acute myocardial infarction incidence. Ran simulation after making changes to configuration.

--- src/vivarium_nih_us_cvd/components/__init__.py
+++ src/vivarium_nih_us_cvd/components/__init__.py
@@ -2,11 +2,6 @@ from .disease import IschemicHeartDiseaseAndHeartFailure, IschemicStroke
 from .effects import InterventionAdherenceEffect
 from .healthcare_utilization import HealthcareUtilization
 from .interventions import LinearScaleUp
-from .observers import (
-    CategoricalColumnObserver,
-    ContinuousRiskObserver,
-    HealthcareVisitObserver,
-    ResultsStratifier,
-)
+from .paf_observer import PAFObserver
 from .risks import AdjustedRisk, TruncatedRisk
 from .treatment import Treatment
