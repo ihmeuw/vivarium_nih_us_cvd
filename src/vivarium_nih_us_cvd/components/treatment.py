16416a4a9bd3b6740173ef0d5a0eb006f1382071
Hussain Jafari <hussainsjafari@gmail.com>
2023-03-28 11:11:53 -0500

    update ldl ladder to include ascvd score (#94)
        Category: implementation
        JIRA issue: MIC-3685
        Research reference: LDL-C Treatment Ramp
    
    Changes and notes
    Add a check for high ASCVD when ramping up medication for currently treated simulants.
    
    Verification and Testing
    Checked ASVCD scores of simulants whose medications were changing and confirmed that they were not lower than 7.5. Also checked other values were correct for people in that specific ramp.

--- src/vivarium_nih_us_cvd/components/treatment.py
+++ src/vivarium_nih_us_cvd/components/treatment.py
@@ -656,10 +656,12 @@ class Treatment:
             to_prescribe_e
         )
         # [Treatment ramp ID G] Simulants who overcome therapeutic inertia, have
-        # elevated LDLC, and are currently medicated
-        to_prescribe_g = overcome_therapeutic_inertia.intersection(
-            currently_medicated
-        ).difference(low_ldlc)
+        # elevated LDLC, have elevated ASCVD, and are currently medicated
+        to_prescribe_g = (
+            overcome_therapeutic_inertia.intersection(currently_medicated)
+            .difference(low_ldlc)
+            .difference(low_ascvd)
+        )
 
         # Prescribe initial medications
         df_newly_prescribed = pd.DataFrame(index=newly_prescribed)
