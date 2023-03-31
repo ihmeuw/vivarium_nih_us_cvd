dbd5e14e0a28c3c1e6b42239813fb7a66d629028
hussain-jafari <hussainsjafari@gmail.com>
2023-03-31 09:49:21 -0500

    black and isort

--- src/vivarium_nih_us_cvd/components/__init__.py
+++ src/vivarium_nih_us_cvd/components/__init__.py
@@ -2,6 +2,11 @@ from .disease import IschemicHeartDiseaseAndHeartFailure, IschemicStroke
 from .effects import InterventionAdherenceEffect
 from .healthcare_utilization import HealthcareUtilization
 from .interventions import LinearScaleUp
-from .observers import CategoricalColumnObserver, ContinuousRiskObserver, HealthcareVisitObserver, ResultsStratifier
+from .observers import (
+    CategoricalColumnObserver,
+    ContinuousRiskObserver,
+    HealthcareVisitObserver,
+    ResultsStratifier,
+)
 from .risks import AdjustedRisk, CategoricalSBPRisk, TruncatedRisk
 from .treatment import Treatment
