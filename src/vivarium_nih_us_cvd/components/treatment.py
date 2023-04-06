f5fd9309fce2a8b0ff90c51517b7b93287ba9d8f
hussain-jafari <hussainsjafari@gmail.com>
2023-04-06 13:14:33 -0500

    remove bmi from treatment

--- src/vivarium_nih_us_cvd/components/treatment.py
+++ src/vivarium_nih_us_cvd/components/treatment.py
@@ -41,7 +41,6 @@ class Treatment:
         self.outreach = builder.value.get_value(data_values.PIPELINES.OUTREACH_EXPOSURE)
         self.polypill = builder.value.get_value(data_values.PIPELINES.POLYPILL_EXPOSURE)
         self.lifestyle = builder.value.get_value(data_values.PIPELINES.LIFESTYLE_EXPOSURE)
-        self.bmi = builder.value.get_value(data_values.PIPELINES.BMI_EXPOSURE)
         self.fpg = builder.value.get_value(data_values.PIPELINES.FPG_EXPOSURE)
 
         self.sbp_treatment_map = self._get_sbp_treatment_map()
