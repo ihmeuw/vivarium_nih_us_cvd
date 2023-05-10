8b28b2c702556f6e3ec6bf91c4bf86f454b57d05
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2023-05-10 16:13:44 -0700

    implement poc configuration of ischemic stroke (#117)

--- src/vivarium_nih_us_cvd/constants/paths.py
+++ src/vivarium_nih_us_cvd/constants/paths.py
@@ -11,6 +11,8 @@ MODEL_SPEC_DIR = BASE_DIR / "model_specifications"
 RESULTS_ROOT = Path(f"/share/costeffectiveness/results/{metadata.PROJECT_NAME}/")
 DATA_ROOT = BASE_DIR / "data"
 
+CAUSE_RISK_CONFIG = BASE_DIR / "configuration" / "cause_risk.yaml"
+
 
 class __Filepaths(NamedTuple):
     """Specific filepaths container"""
