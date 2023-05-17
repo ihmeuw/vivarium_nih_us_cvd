67666fec825946903732671bff5ee2466f47c4fb
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2023-05-16 17:32:52 -0700

    remove unneeded test

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,16 +1,4 @@
 # Sample Test passing with nose and pytest
-from vivarium import InteractiveContext
-
-from vivarium_nih_us_cvd.constants import paths
-
 
 def test_pass():
     assert True, "dummy sample test"
-
-
-def test():
-    sim = InteractiveContext(paths.MODEL_SPEC_DIR / "nih_us_cvd.yaml", setup=False)
-    sim.configuration.input_data.artifact_path = "/home/rmudambi/scratch/alabama.hdf"
-    sim.setup()
-    sim.step()
-    sim.step()
