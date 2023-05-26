f90056dc72ee9ea81e040a41826240d0d4242cb3
hussain-jafari <hussainsjafari@gmail.com>
2023-05-26 13:10:34 -0700

    PAFs for all 51 states

--- src/vivarium_nih_us_cvd/components/__init__.py
+++ src/vivarium_nih_us_cvd/components/__init__.py
@@ -1,5 +1,5 @@
 from .causes import Causes, IschemicHeartDiseaseAndHeartFailure, IschemicStroke
-from .effects import InterventionAdherenceEffect
+from .effects import InterventionAdherenceEffect, PAFCalculationRiskEffect
 from .healthcare_utilization import HealthcareUtilization
 from .interventions import LinearScaleUp
 from .observers import (
@@ -10,5 +10,6 @@ from .observers import (
     LifestyleObserver,
     ResultsStratifier,
 )
+from .paf_observer import PAFObserver
 from .risks import AdjustedRisk, CategoricalSBPRisk, TruncatedRisk
 from .treatment import Treatment
