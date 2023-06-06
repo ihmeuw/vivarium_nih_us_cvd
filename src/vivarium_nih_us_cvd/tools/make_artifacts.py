aeee3f6a0886c877aa72efd638a3949df514b557
Steve Bachmeier <23350991+stevebachmeier@users.noreply.github.com>
2023-06-06 10:13:57 -0600

    better handle existing artifact checks (#127)

--- src/vivarium_nih_us_cvd/tools/make_artifacts.py
+++ src/vivarium_nih_us_cvd/tools/make_artifacts.py
@@ -48,11 +48,9 @@ def check_for_existing(
         ]
     )
     locations = set([sanitize_location(loc) for loc in metadata.LOCATIONS])
+    location = sanitize_location(location)
     existing = locations.intersection(existing_artifacts)
-
-    if existing:
-        if location != "all":
-            existing = [sanitize_location(location)]
+    if existing and (location == "all" or location in existing):
         if not append:
             click.confirm(
                 f"Existing artifacts found for {existing}. Do you want to delete and rebuild?",
