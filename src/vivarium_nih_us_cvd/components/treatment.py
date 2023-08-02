d892cc2b618d49198c0677bb32eefa480c8302fd
Hussain Jafari <hussainsjafari@gmail.com>
2023-08-02 13:16:15 -0500

    Update SBP inertia state table properly (#146)
        Category: bugfix
        JIRA issue: MIC-4263
        Research reference: SBP Treatment Ramp
    
    Changes and notes
    Update entire state table at initialization by passing updated information about visitors.
    
    Verification and Testing
    Ran simulation for a few time steps.

--- src/vivarium_nih_us_cvd/components/treatment.py
+++ src/vivarium_nih_us_cvd/components/treatment.py
@@ -631,6 +631,7 @@ class Treatment:
                 [
                     data_values.COLUMNS.SBP_MEDICATION,
                     data_values.COLUMNS.LDLC_MEDICATION,
+                    data_values.COLUMNS.SBP_THERAPEUTIC_INERTIA_PROPENSITY,
                 ]
             ]
         )
@@ -680,7 +681,7 @@ class Treatment:
         sbp_prescription_inertia_propensity.loc[
             changed_prescription_last_time
         ] = self.randomness.get_draw(
-            changed_prescription_last_time, additional_key="dynamic_inertia_propensity"
+            changed_prescription_last_time, additional_key="dynamic_sbp_inertia_propensity"
         )
         updated_overcome_prescription_inertia = pop_visitors[
             sbp_prescription_inertia_propensity > data_values.SBP_THERAPEUTIC_INERTIA
@@ -763,8 +764,10 @@ class Treatment:
                 high_sbp.union(history_mi_or_is.difference(low_sbp))
             )
 
-        # Update state table to have new propensities
-        self.population_view.update(sbp_prescription_inertia_propensity)
+        # Update propensity in visitors
+        pop_visitors[
+            data_values.COLUMNS.SBP_THERAPEUTIC_INERTIA_PROPENSITY
+        ] = sbp_prescription_inertia_propensity
 
         return pop_visitors, maybe_enroll
 
