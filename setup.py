11f6da230f1558f1cdcb62f77ab46466f8425ee8
Steve Bachmeier <sbachmei@uw.edu>
2023-09-28 11:15:39 -0700

    tweak extras_require

--- setup.py
+++ setup.py
@@ -48,9 +48,9 @@ if __name__ == "__main__":
         install_requires=install_requirements,
         extras_require={
             "test": test_requirements,
-            "data": data_requirements,
             "cluster": cluster_requirements,
-            "dev": test_requirements + data_requirements + cluster_requirements,
+            "data": data_requirements + cluster_requirements, 
+            "dev": test_requirements + cluster_requirements,
         },
         zip_safe=False,
         entry_points="""
